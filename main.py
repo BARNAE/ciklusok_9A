b94429b475c7980ef92f1c41748b65a861f93eb0
sultiszzsombor <fakukac008@gmail.com>
2023-05-02 11:36:11 +0200

    Fehér Barna kiszámoltam hogy a gyémánt karátja mennyi

--- main.py
+++ main.py
@@ -20,16 +20,21 @@ if __name__ == '__main__':
     print('átlag: ' + str(atlag))
 
 
-    for gyemant in gyemantok.iterrows():
-        carat = gyemant[1]['carat']
-        if (carat > atlag):
-            print(gyemant[1])
+
     szum = 0
+
 
     for gyemant in gyemantok.iterrows():
         pass
         # ha a gyémánt színe 'H'
         # akkor addjuk össze a price oszolopból az értékeket
+
+    for gyemant in gyemantok.iterrows():
+        color = gyemant[1]['color']
+        price = gyemant[1]['price']
+        if color == 'H':
+            szum += price
+            # print(gyemant)
 
     print("Összes ára: " + str(szum))
 
